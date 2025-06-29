5f36ffeb4dd298a1b8d4e490106646674a8025a1
Cesar Freire <iceesar@live.com>
2025-06-29 16:08:17 -0300

    feat: Criando primeiro schema (aula 02)

--- fastapi_do_zero/app.py
+++ fastapi_do_zero/app.py
@@ -1,8 +1,17 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
 
-app = FastAPI()
+from fastapi_do_zero.schemas import Message
+
+app = FastAPI(
+    title='FastAPI do Zero',
+    description='Aplicação do curso de FastAPI do Zero',
+    version='0.1.0',
+    contact={'name': 'César Freire', 'email': 'iceesar@live.com'},
+)
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo!'}
