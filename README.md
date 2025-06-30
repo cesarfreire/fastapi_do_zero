94ea9e575d16845cfd7646ea8fc17701e47b3b99
Cesar Freire <iceesar@live.com>
2025-06-30 15:15:38 -0300

    feat: SQLAchemy e Alembic (aula 04)

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # FastAPI 2025
+
+## Migrations
+
+Executa migration:
+
+```bash
+alembic upgrade head
+```
+
+Desfazer última migration:
+
+```bash
+alembic downgrade -1
+```
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
+from contextlib import contextmanager
+from datetime import datetime
+
 import pytest
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, event
+from sqlalchemy.orm import Session
 
 from fastapi_do_zero.app import app
+from fastapi_do_zero.models import table_registry
 
 
 @pytest.fixture
@@ -12,3 +18,37 @@ def client():
     instantiate it multiple times.
     """
     return TestClient(app)
+
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
+
+
+@pytest.fixture
+def mock_db_time():
+    """
+    Fixture to mock the database time for testing purposes.
+    It sets a fixed time for the `created_at` field in the User model.
+    """
+    return _mock_db_time
+
+
+@contextmanager
+def _mock_db_time(*, model, time=datetime(2024, 1, 1)):
+    def fake_time_hook(mapper, connection, target):
+        if hasattr(target, 'created_at'):
+            target.created_at = time
+        
+        if hasattr(target, 'updated_at'):
+            target.updated_at = time
+
+    event.listen(model, 'before_insert', fake_time_hook)
+    yield time
+    event.remove(model, 'before_insert', fake_time_hook)
